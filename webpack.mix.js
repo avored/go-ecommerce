35f3d80d1189743cbbf9522dce9ec11c234cb309
indpurvesh <ind.purvesh@gmail.com>
2022-05-29 10:33:52 +1200

    html template set

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,8 @@
 let mix = require('laravel-mix');
 
+
+/******** AVORED COPY IMAGES  **********/
+mix.copyDirectory('resources/img', "public/img");
+
+
 mix.js('resources/js/app.js', 'public/js/app.js').setPublicPath('public');
