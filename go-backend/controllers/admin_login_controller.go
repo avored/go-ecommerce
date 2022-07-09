7438b3d4614a9e7db3c5b57e6e45b55165c96ede
indpurvesh <ind.purvesh@gmail.com>
2022-07-10 10:00:16 +1200

    admin login with user validation

--- go-backend/controllers/admin_login_controller.go
+++ go-backend/controllers/admin_login_controller.go
@@ -4,17 +4,36 @@ import (
 
 	"github.com/avored/go-ecommerce/services"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func AdminLoginHandler(ctx *gin.Context) {
-	email := "admin@admin.com"
+	email := ctx.PostForm("email")
+
 	adminUserModel := services.FetchAdminUserByEmail(ctx, email)
-	
-	// token := "sjsjhekhjskjhsnjkdhjiskjhsjklepoisoiuapol7"
-	// AdminUSerModel := "tests"
+
+	match := CheckPasswordHash(ctx.PostForm("password"), adminUserModel.Password)
+	var token  string = "INVALID_TOKEN"
+	if match {
+		token = "my_demo_valid_token_add_middleware_jwt_token_via_gin_framework"
+	}
+
 	ctx.JSON(200,
 		gin.H{
 			"admin_user": adminUserModel,
+			"token": token,
 		},
 	)
+}
+
+
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
